7eb5eece148484b14afeb62f683b7018adf870de
Grzegorz Karch <gkarch@nvidia.com>
2019-06-26 08:57:23 -0700

    changed number of epochs in train scripts; removed number of epochs from readme for the audio samples

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/train_waveglow.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/train_waveglow.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-python -m multiproc train.py -m WaveGlow -o ./output/ -lr 1e-4 --epochs 2001 -bs 8 --segment-length  8000 --weight-decay 0 --grad-clip-thresh 65504.0 --epochs-per-checkpoint 50 --cudnn-benchmark=True --log-file ./output/nvlog.json --fp16-run
+python -m multiproc train.py -m WaveGlow -o ./output/ -lr 1e-4 --epochs 1000 -bs 8 --segment-length  8000 --weight-decay 0 --grad-clip-thresh 65504.0 --epochs-per-checkpoint 50 --cudnn-benchmark=True --log-file ./output/nvlog.json --fp16-run
