01a2969e9bca71d2d87e0ac45cc4cd03aa138af0
machineko <42299606+machineko@users.noreply.github.com>
2020-04-15 02:06:40 +0200

    fix dropout in prenet
    fix bug when dropout stay in prenet even in eval mode

--- PyTorch/SpeechSynthesis/Tacotron2/tacotron2/model.py
+++ PyTorch/SpeechSynthesis/Tacotron2/tacotron2/model.py
@@ -120,13 +120,14 @@ class Prenet(nn.Module):
     def __init__(self, in_dim, sizes):
         super(Prenet, self).__init__()
         in_sizes = [in_dim] + sizes[:-1]
+        self.dropout = nn.Dropout(p=0.5)
         self.layers = nn.ModuleList(
             [LinearNorm(in_size, out_size, bias=False)
              for (in_size, out_size) in zip(in_sizes, sizes)])
 
     def forward(self, x):
         for linear in self.layers:
-            x = F.dropout(F.relu(linear(x)), p=0.5, training=True)
+            x = self.dropout(F.relu(linear(x)))
         return x
 
 
