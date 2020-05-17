b66743926a76c247997b49d255d199dde6cb962f
Sharath T S <ts.sharath18@gmail.com>
2020-05-16 22:50:03 -0700

    Update utils.py

--- PyTorch/LanguageModeling/BERT/utils.py
+++ PyTorch/LanguageModeling/BERT/utils.py
@@ -21,6 +21,13 @@ def get_rank():
         return 0
     return dist.get_rank()
 
+def get_world_size():
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size()
+
 def is_main_process():
     return get_rank() == 0
 
