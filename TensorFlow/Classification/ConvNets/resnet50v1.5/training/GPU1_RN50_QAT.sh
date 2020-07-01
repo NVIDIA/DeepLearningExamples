00bde500815d490925e430d7016f0ac1e898f2da
Dheeraj Peri <peri.dheeraj@gmail.com>
2020-07-01 13:16:58 -0700

    Remove folder for QAT

---
+++ TensorFlow/Classification/ConvNets/resnet50v1.5/training/GPU1_RN50_QAT.sh
@@ -0,0 +1,4 @@
+# This script does Quantization aware training of Resnet-50 by finetuning on the pre-trained model using 1 GPU and a batch size of 32.
+# Usage ./GPU1_RN50_QAT.sh <path to the pre-trained model> <path to dataset> <path to results directory>
+
+python main.py --mode=train_and_evaluate --batch_size=32 --lr_warmup_epochs=1 --quantize --symmetric --use_qdq --label_smoothing 0.1 --lr_init=0.00005 --momentum=0.875 --weight_decay=3.0517578125e-05 --finetune_checkpoint=$1 --data_dir=$2 --results_dir=$3 --num_iter 10 --data_format NHWC
