4cb58b61c70045151f1cfb2ee6c8b167f86a2f39
Dheeraj Peri <peri.dheeraj@gmail.com>
2020-07-06 12:01:17 -0700

    Add licenses to new files

--- TensorFlow/Classification/ConvNets/resnet50v1.5/training/GPU1_RN50_QAT.sh
+++ TensorFlow/Classification/ConvNets/resnet50v1.5/training/GPU1_RN50_QAT.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Copyright (c) 2020 NVIDIA CORPORATION. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#       http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This script does Quantization aware training of Resnet-50 by finetuning on the pre-trained model using 1 GPU and a batch size of 32.
 # Usage ./GPU1_RN50_QAT.sh <path to the pre-trained model> <path to dataset> <path to results directory>
 
