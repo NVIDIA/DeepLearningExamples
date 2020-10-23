c32e7ebba07c165eb3a909b5a750c2a1cd63d253
Szymon Migacz <1934379+szmigacz@users.noreply.github.com>
2020-10-23 15:42:59 -0700

    [TXL/PyT] Fixed issue with AMP training together with gradient accumulation

--- PyTorch/LanguageModeling/Transformer-XL/pytorch/data_utils.py
+++ PyTorch/LanguageModeling/Transformer-XL/pytorch/data_utils.py
@@ -46,7 +46,7 @@ class LMOrderedIterator(object):
         data = data[:n_step * bsz]
 
         # Evenly divide the data across the bsz batches.
-        self.data = data.view(bsz, -1).t().contiguous()
+        self.data = data.view(bsz, -1).t().contiguous().pin_memory()
 
         if mem_len and warmup:
             self.warmup_batches = (mem_len + bptt - 1) // bptt
@@ -83,8 +83,8 @@ class LMOrderedIterator(object):
         end_idx = i + seq_len
         beg_idx = max(0, i - self.ext_len)
 
-        data = self.data[beg_idx:end_idx].to(self.device)
-        target = self.data[i+1:i+1+seq_len].to(self.device)
+        data = self.data[beg_idx:end_idx].to(self.device, non_blocking=True)
+        target = self.data[i+1:i+1+seq_len].to(self.device, non_blocking=True)
 
         if self.mem_len and self.warmup:
             warm = i >= self.warmup_elems
