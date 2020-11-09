c9846ca61871132f8f506bc89db49a51d6a96135
Lukasz Pierscieniewski <lukaszp@nvidia.com>
2020-11-09 13:37:14 +0100

    Fix merge issues

--- PyTorch/Classification/ConvNets/triton/deployer.py
+++ PyTorch/Classification/ConvNets/triton/deployer.py
@@ -61,7 +61,6 @@ def initialize_model(args):
         model.load_state_dict(
             {k.replace("module.", ""): v for k, v in state_dict.items()}
         )
-        model.load_state_dict(state_dict)
     return model.half() if args.fp16 else model
 
 
