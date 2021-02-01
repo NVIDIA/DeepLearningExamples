d07eb83bdbe7bb1ad0db402b2baf417acb896981
kkudrynski <kkudrynski@nvidia.com>
2021-02-01 15:34:21 +0100

    [nnUnet/PyT] Notebook and minor corrections

--- PyTorch/Segmentation/nnUNet/data_preprocessing/preprocessor.py
+++ PyTorch/Segmentation/nnUNet/data_preprocessing/preprocessor.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import itertools
 import json
 import math
@@ -11,7 +26,8 @@ from joblib import Parallel, delayed
 from skimage.transform import resize
 from utils.utils import get_task_code, make_empty_dir
 
-from data_preprocessing.configs import ct_max, ct_mean, ct_min, ct_std, patch_size, spacings, task
+from data_preprocessing.configs import (ct_max, ct_mean, ct_min, ct_std,
+                                        patch_size, spacings, task)
 
 
 class Preprocessor:
