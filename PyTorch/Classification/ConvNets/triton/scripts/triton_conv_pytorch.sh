3395921c06998946e2535037c0f76c29f7a7f926
Matthew Yeung <yyygggfff@hotmail.com>
2021-03-08 16:50:06 +0800

    update path

--- PyTorch/Classification/ConvNets/triton/scripts/triton_conv_pytorch.sh
+++ PyTorch/Classification/ConvNets/triton/scripts/triton_conv_pytorch.sh
@@ -1,5 +1,5 @@
 docker run -it --rm \
   --shm-size=1g --ulimit memlock=-1 \
   --ulimit stack=67108864 --net=host \
-  -v /Users/gingfungyeung/Dev/DeepLearningExamples/PyTorch/Classification/ConvNets:/workspace/rn50 \
+  -v $(pwd):/workspace/rn50 \
   nv-conv-pytorch-exp bash
