b946389828fa7d6f7d11c6ee2e359d43c011f27b
Andrzej Sulecki <asulecki@nvidia.com>
2021-03-22 11:42:01 +0100

    Fix missing multiproc in example training script

--- PyTorch/Classification/ConvNets/resnet50v1.5/training/AMP/DGXA100_resnet50_AMP_250E.sh
+++ PyTorch/Classification/ConvNets/resnet50v1.5/training/AMP/DGXA100_resnet50_AMP_250E.sh
@@ -1,1 +1,1 @@
-python ./launch.py --model resnet50 --precision AMP --mode convergence --platform DGXA100 /imagenet --workspace ${1:-./} --raport-file raport.json
+python ./multiproc.py --nproc_per_node 8 ./launch.py --model resnet50 --precision AMP --mode convergence --platform DGXA100 /imagenet --workspace ${1:-./} --raport-file raport.json
