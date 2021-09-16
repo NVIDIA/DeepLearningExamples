69925cc1c55ecba5b82b706c8e4761fb749a93c1
Meeeeee6623 <c.benjaminsemail1@gmail.com>
2021-09-15 21:40:36 -0400

    add cifar100 to recordio for mxnet

--- MxNet/Classification/RN50v1.5/MxNet_Cifar100.sh
+++ MxNet/Classification/RN50v1.5/MxNet_Cifar100.sh
@@ -1,2 +1,2 @@
 # nvidia-docker run --rm -it --ipc=host -v /work/chauhans/cifar100:/cifar100 nvidia_rn50_mx
-./runner -n 2 -b 192 --num-epochs 90 --num-classes 100 --mode train_val --amp --dllogger-log /cifar100/MxNet/MX_Cifar100.log --workspace /cifar100/MxNet --data-backend synthetic --image-shape 3,32,32 --data-root /cifar100/
+./runner -n 2 -b 192 --num-epochs 90 --num-classes 100 --mode train_val --amp --dllogger-log /cifar100/MxNet/MX_Cifar100.log --workspace /cifar100/MxNet --data-backend synthetic --image-shape 3,32,32 --num-examples 50000 --data-root /cifar100/
