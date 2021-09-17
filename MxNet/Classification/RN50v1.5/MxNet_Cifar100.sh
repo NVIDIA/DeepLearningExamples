4a4e4459e68e3d3c326bee0268d4b7b26c39af7b
Meeeeee6623 <c.benjaminsemail1@gmail.com>
2021-09-17 14:30:47 -0400

    add pytorch cifar100 5 runs script

--- MxNet/Classification/RN50v1.5/MxNet_Cifar100.sh
+++ MxNet/Classification/RN50v1.5/MxNet_Cifar100.sh
@@ -2,7 +2,7 @@
 for (( i = 0; i < 6; i++ )); do # 5 runs
     mkdir /cifar100/MxNet/run_"${i}"/
     echo "Created Folder run_${i}"
-    ./runner -n 2 -b 192 --num-epochs 90 --num-classes 100 --mode train_val --amp --dllogger-log /cifar100/MxNet/run_"${i}"/run_"${i}".log --workspace /cifar100/MxNet/run_"${i}"/ --data-backend mxnet --image-shape 3,32,32 --data-root /cifar100 --warmup 0
+    ./runner -n 2 -b 192 --num-epochs 90 --num-classes 100 --mode train_val --amp --dllogger-log /cifar100/MxNet/run_"${i}"/run_"${i}".log --workspace /cifar100/MxNet/run_"${i}"/ --data-backend mxnet --data-root /cifar100 --warmup 0
     echo "Run ${i} done"
 done
 
