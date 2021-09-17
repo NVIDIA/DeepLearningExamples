cf1c7e6f303b3efc11cdb3f196e92433fad57897
Meeeeee6623 <c.benjaminsemail1@gmail.com>
2021-09-17 15:06:10 -0400

    add tf 3 runs script

---
+++ TensorFlow/Classification/ConvNets/tf_3_runs.sh
@@ -0,0 +1,13 @@
+for (( n = 0; i < 3; n++ )); do
+  for (( i = 0; i < 91; i++ )); do
+      mkdir /imagenet/TensorFlow/run_"${n}"/epoch_"${i}"/
+      echo "Created Folder epoch_${i}"
+      mpiexec --allow-run-as-root --bind-to socket -np 2 python3 main.py --arch=resnet50 --mode=train_and_evaluate \
+      --num_iter=i --batch_size=192 --warmup_steps=0 --lr_warmup_epochs=0 --model_dir=/imagenet/TensorFlow/model     \
+      --data_dir=/imagenet/tfrecords --data_idx_dir=/imagenet/dali_idx     \
+      --results_dir=/imagenet/TensorFlow/run_"${n}"/epoch_"${i}"/          \
+      --export_dir=/imagenet/TensorFlow/model --weight_init=fan_in --amp --log_filename run_"${n}"epoch_"${i}".json
+      echo "Epoch ${i} done"
+  done
+  echo "Run ${n} done"
+done
