b14d2881255c918ffd030e6f73f9aae5d43de271
Meeeeee6623 <c.benjaminsemail1@gmail.com>
2021-11-05 16:30:32 -0400

    remove scripts

--- MxNet/Classification/RN50v1.5/Cifar100ToRecordIO.sh
+++
@@ -1,4 +0,0 @@
-python /opt/mxnet/tools/im2rec.py --list --recursive train /cifar100/train
-python /opt/mxnet/tools/im2rec.py --list --recursive val /cifar100/val
-python /opt/mxnet/tools/im2rec.py --pass-through --num-thread 40 train /cifar100/train
-python /opt/mxnet/tools/im2rec.py --pass-through --num-thread 40 val /cifar100/val
--- MxNet/Classification/RN50v1.5/MxNet_Cifar100.sh
+++
@@ -1,7 +0,0 @@
-# nvidia-docker run --rm -it --ipc=host -v /work/chauhans/cifar100:/cifar100 mxnet_cifar100
-for (( i = 0; i < 6; i++ )); do # 5 runs
-    mkdir /cifar100/MxNet/run_"${i}"/
-    echo "Created Folder run_${i}"
-    ./runner -n 2 -b 192 --num-epochs 90 --num-classes 100 --mode train_val --amp --dllogger-log /cifar100/MxNet/run_"${i}"/run_"${i}".log --workspace /cifar100/MxNet/run_"${i}"/ --data-backend mxnet --data-root /cifar100 --warmup 0
-    echo "Run ${i} done"
-done
