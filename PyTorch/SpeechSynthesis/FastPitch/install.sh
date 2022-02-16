f8092f7e1024f29cc00482fa1144e2f71b927dfc
evdv <emelie.vdvreken@gmail.com>
2022-02-16 16:54:44 +0000

    Change conda install command

--- PyTorch/SpeechSynthesis/FastPitch/install.sh
+++ PyTorch/SpeechSynthesis/FastPitch/install.sh
@@ -17,7 +17,7 @@ then
   cd /disk/scratch1/${USER}/
   wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh
   echo "Change install location to /disk/scratch1/${USER}"
-  ./Miniconda3-py39_4.9.2-Linux-x86_64.sh
+  bash ./Miniconda3-py39_4.9.2-Linux-x86_64.sh
   source ~/.bashrc
 fi
 
