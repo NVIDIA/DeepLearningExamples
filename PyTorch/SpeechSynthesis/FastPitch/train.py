6d288437153a9eae7077f9b25c5b4e325fda4206
Emelie Van De Vreken <emelie.vdvreken@gmail.com>
2022-04-14 11:33:13 +0100

    Add pitch, energy, and duration losses (#11)
    To Weights and Biases logging

--- PyTorch/SpeechSynthesis/FastPitch/train.py
+++ PyTorch/SpeechSynthesis/FastPitch/train.py
@@ -425,6 +425,9 @@ def validate(model, criterion, valset, batch_size, collate_fn, distributed_run,
     log({
         'loss/validation-loss': val_meta['loss'].item(),
         'mel-loss/validation-mel-loss': val_meta['mel_loss'].item(),
+        'pitch-loss/validation-pitch-loss': val_meta['pitch_loss'].item(),
+        'energy-loss/validation-energy-loss': val_meta['energy_loss'].item(),
+        'dur-loss/validation-dur-error': val_meta['duration_predictor_loss'].item(),
         'validation-frames per s': num_frames.item() / val_meta['took'],
         'validation-took': val_meta['took'],
     }, rank)
@@ -610,6 +613,9 @@ def main():
 
         epoch_loss = 0.0
         epoch_mel_loss = 0.0
+        epoch_pitch_loss = 0.0
+        epoch_energy_loss = 0.0
+        epoch_dur_loss = 0.0
         epoch_num_frames = 0
         epoch_frames_per_sec = 0.0
 
@@ -703,11 +709,18 @@ def main():
 
                 iter_mel_loss = iter_meta['mel_loss'].item()
                 iter_kl_loss = iter_meta['kl_loss'].item()
+                iter_pitch_loss = iter_meta['pitch_loss'].item()
+                iter_energy_loss = iter_meta['energy_loss'].item()
+                iter_dur_loss = iter_meta['duration_predictor_loss'].item()
                 iter_time = time.perf_counter() - iter_start_time
                 epoch_frames_per_sec += iter_num_frames / iter_time
                 epoch_loss += iter_loss
                 epoch_num_frames += iter_num_frames
                 epoch_mel_loss += iter_mel_loss
+                epoch_pitch_loss += iter_pitch_loss
+                epoch_energy_loss += iter_energy_loss
+                epoch_dur_loss += iter_dur_loss
+
                 if epoch_iter % 5 == 0:
                     log({
                         'epoch': epoch,
@@ -718,6 +731,9 @@ def main():
                         'mel-loss/mel_loss': iter_mel_loss,
                         'kl_loss': iter_kl_loss,
                         'kl_weight': kl_weight,
+                        'pitch-loss/pitch_loss': iter_pitch_loss,
+                        'energy-loss/energy_loss': iter_energy_loss,
+                        'dur-loss/dur_loss': iter_dur_loss,
                         'frames per s': iter_num_frames / iter_time,
                         'took': iter_time,
                         'lrate': optimizer.param_groups[0]['lr'],
@@ -738,6 +754,9 @@ def main():
             'epoch': epoch,
             'loss/epoch_loss': epoch_loss,
             'mel-loss/epoch_mel_loss': epoch_mel_loss,
+            'pitch-loss/epoch_pitch_loss': epoch_pitch_loss,
+            'energy-loss/epoch_energy_loss': epoch_energy_loss,
+            'dur-loss/epoch_dur_loss': epoch_dur_loss,
             'epoch_frames per s': epoch_num_frames / epoch_time,
             'epoch_took': epoch_time,
         }, args.local_rank)
