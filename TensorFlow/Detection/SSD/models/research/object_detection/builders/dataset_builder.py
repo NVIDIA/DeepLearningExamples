007bd6d3de79731c061a28adec21fb2cb558c76e
akalankag <boney.ag@gmail.com>
2022-09-22 15:35:53 -0600

    change batch_and_drop_remainder to batch(batch_size, drop_remainder=True)

--- TensorFlow/Detection/SSD/models/research/object_detection/builders/dataset_builder.py
+++ TensorFlow/Detection/SSD/models/research/object_detection/builders/dataset_builder.py
@@ -162,8 +162,7 @@ def build(input_reader_config, batch_size=None, transform_input_data_fn=None, mu
         process_fn,
         num_parallel_calls=num_parallel_calls)
     if batch_size:
-      dataset = dataset.apply(
-          tf.contrib.data.batch_and_drop_remainder(batch_size))
+      dataset = dataset.batch(batch_size, drop_remainder=True)
     dataset = dataset.prefetch(input_reader_config.num_prefetch_batches)
     return dataset
 
