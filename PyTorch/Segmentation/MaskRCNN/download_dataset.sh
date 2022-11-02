4e9bae8393bb34d0633190b43ca5a10f801793e5
Shriya Balaji Palsamudram <spalsamudram@nvidia.com>
2022-11-02 13:08:46 -0700

    [MRCNN/PYT] Fix coco data download

--- PyTorch/Segmentation/MaskRCNN/download_dataset.sh
+++ PyTorch/Segmentation/MaskRCNN/download_dataset.sh
@@ -1,6 +1,5 @@
 DATA_DIR=$1
 
-wget -c https://dl.fbaipublicfiles.com/detectron/coco/coco_annotations_minival.tgz
 wget -c http://images.cocodataset.org/zips/train2017.zip
 wget -c http://images.cocodataset.org/zips/val2017.zip
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
@@ -9,16 +8,12 @@ if md5sum -c hashes.md5
 then
 	echo "DOWNLOAD PASSED"
 	# mkdir $DATA_DIR
-	mv coco_annotations_minival.tgz $DATA_DIR
 	mv train2017.zip $DATA_DIR
 	mv val2017.zip $DATA_DIR
 	mv annotations_trainval2017.zip $DATA_DIR
 
 	cd $DATA_DIR
-	dtrx --one=here coco_annotations_minival.tgz
 	dtrx --one=here annotations_trainval2017.zip
-	mv annotations.1/* annotations/
-
 	dtrx train2017.zip
 	dtrx val2017.zip
 
